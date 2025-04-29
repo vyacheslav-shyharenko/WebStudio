e13d683000f905de015b455475019242bdfee88c
Vyacheslav Shyharenko <shyharenkovyacheslav.dev@gmail.com>
2025-04-29 18:23:32 +0300

    add 2 seections

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import path from 'path';
+import FullReload from 'vite-plugin-full-reload';
+import viteImagemin from 'vite-plugin-imagemin';
+
+export default defineConfig(({ command }) => ({
+  root: 'src',
+  base: './',
+  publicDir: '../public',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: command === 'serve',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+
+  plugins: [
+    FullReload(['src/**/*.html', 'src/**/*.scss', 'src/**/*.js']),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      svgo: {
+        plugins: [
+          { name: 'removeViewBox', active: false },
+          { name: 'cleanupIDs', active: false },
+        ],
+      },
+    }),
+  ],
+}));
